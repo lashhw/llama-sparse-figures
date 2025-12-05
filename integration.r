23a323f4ef61dfe939563efd7e2da9aa668f8b14
Yen-Chieh Huang <lashhw81@gmail.com>
2025-12-05 15:50:24 +0800

    add integration

---
+++ integration.r
@@ -0,0 +1,50 @@
+library(tidyverse)
+library(patchwork)
+
+data <- read_csv("data/integration.csv") %>%
+  mutate(
+    method = fct_inorder(method)
+  )
+
+panel_info <- data %>%
+  distinct(panel_title, y_label)
+
+fig_list <- map(seq_len(nrow(panel_info)), \(idx) {
+  panel_label <- panel_info$panel_title[[idx]]
+  axis_label <- panel_info$y_label[[idx]]
+
+  data %>%
+    filter(panel_title == panel_label, y_label == axis_label) %>%
+    ggplot(aes(x = kv_budget, y = score, colour = method)) +
+      geom_line(linewidth = 1) +
+      geom_point(size = 2) +
+      scale_colour_manual(
+        values = c("QUEST Only" = "#82B366", "WG-KV + QUEST" = "#D79B00")
+      ) +
+      scale_x_continuous(
+        limits = c(0, 1),
+        breaks = seq(0, 1, by = 0.2),
+        expand = c(0, 0)
+      ) +
+      labs(
+        x = "KV Budget",
+        y = axis_label,
+        title = panel_label
+      ) +
+      coord_cartesian(clip = "off") +
+      theme_minimal(base_size = 14) +
+      theme(
+        plot.title = element_text(size = 13, hjust = 0.5),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 15),
+        axis.text.x = element_text(size = 10, colour = "black"),
+        axis.text.y = element_text(size = 10, colour = "black"),
+        axis.title.x = element_text(size = 11),
+        axis.title.y = element_text(size = 11),
+      )
+})
+
+fig <- wrap_plots(fig_list, ncol = 7, guides = "collect") &
+  theme(legend.position = "bottom")
+
+ggsave("integration.pdf", fig, width = 14.0, height = 4.5, units = "in")
