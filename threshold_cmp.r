7a8cbfb49e7e76497d5505e44f54c9a24c64ac8e
Yen-Chieh Huang <lashhw81@gmail.com>
2025-12-24 01:21:25 +0800

    add threshold_cmp.r and update threshold.r

--- threshold_cmp.r
+++ threshold_cmp.r
@@ -1,10 +1,15 @@
 library(tidyverse)
 
-data <- read_csv("data/threshold.csv") %>%
+data1 <- read_csv("data/threshold.csv") %>%
+  mutate(method = "WG-KV")
+data2 <- read_csv("data/threshold_local1.csv") %>%
+  mutate(method = "WG-KV w/o Local Cache")
+
+data <- bind_rows(data1, data2) %>%
   arrange(lambda, threshold) %>%
   mutate(lambda = factor(lambda, levels = sort(unique(lambda))))
 
-fig <- ggplot(data, aes(x = kv_size, y = distill_loss, colour = lambda)) +
+fig <- ggplot(data, aes(x = kv_size, y = distill_loss, colour = lambda, shape = method)) +
   geom_line(linewidth = 1) +
   geom_point(size = 1.3) +
   scale_colour_brewer(palette = "Dark2") +
@@ -17,24 +22,26 @@ fig <- ggplot(data, aes(x = kv_size, y = distill_loss, colour = lambda)) +
     expand = expansion(mult = c(0, 0.02))
   ) +
   guides(
-    colour = guide_legend(nrow = 2, byrow = TRUE)
+    colour = guide_legend(nrow = 2, byrow = TRUE),
+    shape = guide_legend(override.aes = list(size = 2.5))
   ) +
   labs(
-    x = "Normalized KV Cache Size\n(log scale)",
+    x = "Normalized KV Cache Size (log scale)",
     y = "Distillation Loss",
-    colour = expression(lambda)
+    colour = expression(lambda),
+    shape = NULL
   ) +
-  coord_cartesian(ylim = c(0, NA), clip = "off") +
+  coord_cartesian(ylim = c(0, 0.34), clip = "on") +
   theme_minimal(base_size = 18) +
   theme(
     legend.position = "top",
+    legend.box = "vertical",
     legend.title = element_text(size = 16),
     legend.text = element_text(size = 14),
     axis.text.x = element_text(size = 13, colour = "black"),
     axis.text.y = element_text(size = 13, colour = "black"),
     axis.title.x = element_text(size = 15),
     axis.title.y = element_text(size = 15),
-    plot.margin = margin(r = 50)
   )
 
-ggsave("threshold.pdf", fig, width = 3.7, height = 4.0, units = "in")
+ggsave("threshold_cmp.pdf", fig, width = 5, height = 4.5, units = "in")
